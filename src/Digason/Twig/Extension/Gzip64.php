571bb8973aa95367f92a065f3f44b9b59e5749a9
Jason Northrup <digason@gmail.com>
2020-07-17 15:17:58 -0500

    🤞

---
+++ src/Digason/Twig/Extension/Gzip64.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Digason\Twig\Extension;
+
+use Twig_Extension;
+use Twig_SimpleFunction;
+
+class PhpFunctionExtension extends Twig_Extension
+{
+	public function getFilters() {
+		return [
+		new \Twig_SimpleFilter('gzip64', [$this, 'gzip64']),
+		new \Twig_SimpleFilter('gunzip64', [$this, 'gunzip64']),
+		];
+	}
+
+	public function gzip64($input) {
+		return base64_encode(gzcompress($input, 9));
+	}
+
+	public function gunzip64($input) {
+		return gzuncompress(base64_dencode($input));
+	}
+
+
+	public function getName()
+	{
+		return 'gzip64';
+	}
+}
